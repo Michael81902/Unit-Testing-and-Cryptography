2642908b4e837db87485a19575a38a5a6882e8ed
Michael81902 <110932982+Michael81902@users.noreply.github.com>
2024-10-03 22:58:35 -0400

    Update test_caesar_decode.py

---
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_caesar_decode.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from main import caesar_encode
+
+class TestCaesarEncode(TestCase):
+    def test_encode(self):
+        self.assertEqual(caesar_encode("MJQQTBTWQI ",  5) , "MJQQTBTWQI" )
